959ed874eabe5db7c1ea04669c830965eceb02ec
Yukino <2651518588@qq.com>
2025-11-10 22:27:01 +0800

    gitignore

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client"/>
 
+// 环境变量
 interface ImportMetaEnv {
     readonly VITE_APP_BASE_API: string;
 }
@@ -8,7 +9,7 @@ interface ImportMeta {
     readonly env: ImportMetaEnv;
 }
 
-
+// vue组件
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
