8daf46d1981aeabecc429f0b8cb7b1839d9dc662
Yukino <2651518588@qq.com>
2025-11-13 22:22:29 +0800

    icon

--- App.vue
+++ App.vue
@@ -13,11 +13,23 @@
 </script>
 
 <style lang="scss">
+	/* 引入自定义图标 */
+
 /* 全局样式 */
 @import "@/uni.scss";
 /* sard-uniapp组件库样式 */
 @import '@/uni_modules/sard-uniapp/index.scss';
 /* sard-uniapp组件库暗色模式 */
 @import '@/uni_modules/sard-uniapp/dark.scss';
+@import '@/assets/icons/icons.css';
 
+/* 暗色模式 */
+@media(prefers-color-scheme: dark) {
+	page {
+		background-color: var(--sar-body-bg);
+	}
+	text {
+		color: var(--sar-secondary-color);
+	}
+}
 </style>
