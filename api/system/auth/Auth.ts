6dce09f1c6ad31f416d9549f503c0ca5458fa31a
Yukino <2651518588@qq.com>
2025-11-14 17:25:14 +0800

    用户登录｜加密相关

---
+++ api/system/auth/Auth.ts
@@ -0,0 +1,24 @@
+import request from "@/utils/Request.ts";
+import type {AuthInfoType} from "@/api/system/auth/type/AuthInfoType.ts";
+
+// 获取用户信息
+export const queryAuthInfo = () => {
+    return request<AuthInfoType>({
+        url: '/system/info',
+        method: 'GET'
+    })
+}
+// 刷新用户数据
+export const reloadData = () => {
+    return request({
+        url: '/system/reloadData',
+        method: 'POST'
+    })
+}
+// 获取公钥
+export const getPublicKey = (requestKey: string) => {
+    return request<string>({
+        url: '/system/publicKey/' + requestKey,
+        method: 'GET'
+    })
+}
